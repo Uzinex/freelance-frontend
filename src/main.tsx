942519cb467c9178c6eeb8423574ce4e83a69878
Uzinex <uzdilovferuz@gmail.com>
2025-09-05 20:53:26 +0500

    Changes frontend

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ApolloProvider } from '@apollo/client';
-import App from './App.tsx';
-import './styles/globals.css';
-import { client } from './api/projectsApi';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ApolloProvider } from "@apollo/client";
+import App from "./App.tsx";
+import "./styles/globals.css";
+import { client } from "./api/apolloClient";  // отдельный Apollo client
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <App />
